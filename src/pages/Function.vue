0079fc52fd1c64e791fdc6c34a6dbd29d14793fa
CapralJack <larionov.anton85@gmail.com>
2020-01-19 21:20:16 +0300

    'firstFromPreviuos'

---
+++ src/pages/Function.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <b-container>
+        
+     <div style="margin-top: 40px; margin-bottom: 40px; text-align: left ">
+        <h1><span style="color: #e16262;">Employee Concept </span>{{'func_oport'|localize}} </h1>
+      </div>
+      <div style="margin: 40px; text-align: left; text-size: ">
+        <p>{{"FunctionPage0"|localize}}</p>
+      </div>
+
+        <b-row align-v="center">
+                   <!-- 1 -->
+          <b-col class="md-4" style=" text-align: left;  " >
+                <h1>{{"FunctionPage1"|localize}}</h1>
+                 <img width="270px" src="../imgs/func1.png">
+
+                 <b-card  >
+                   <p>Повышаем эффективность HR-процессов<br> за счет внедрения мобильных технологий</p>
+                </b-card>
+          </b-col>
+            <!-- 2 -->
+          <b-col  class="md-4" style=" text-align: left;  " >
+                <h1>Адаптация</h1>
+                 <img width="270px" src="../imgs/func1.png">
+
+                 <b-card>
+                 
+                  
+                   <p>Повышаем эффективность HR-процессов<br> за счет внедрения мобильных технологий</p>
+                </b-card>
+          </b-col>
+          <!-- 3 -->
+          <b-col class="md-4" style=" text-align: left;  " >
+                <h1>Адаптация</h1>
+                 <img width="270px" src="../imgs/func1.png">
+
+                 <b-card >
+                 
+                  
+                   <p>Повышаем эффективность HR-процессов<br> за счет внедрения мобильных технологий</p>
+                </b-card>
+          </b-col>
+          
+          
+
+          
+          
+        </b-row>
+        </b-container>
+
+</template>
+
+<script>
+import SiteLayout from '@/layouts/SiteLayout';
+import localizeFilter from '@/filters/localize.filter'
+export default {
+  
+}
+</script>
